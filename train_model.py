70cf8282cca895e90cab204a81e108809d0e247d
Caleb Benton <calebbenton@mac.lan>
2025-07-12 14:32:02 -0400

    added a new ipynb

--- train_model.py
+++ train_model.py
@@ -16,9 +16,17 @@ df.set_index('Datetime', inplace=True)
 # Feature engineering
 df['hour'] = df.index.hour
 df['volatility'] = df['High'] - df['Low']
+df['price_change'] = df['Close']- df['Open']
+df['percent_change'] = (df['Close'] - df['Open']) / df['Open']
+df['rolling_mean_3'] = df['Close'].rolling(window=3).mean()
+df['rolling_std_3'] = df['Close'].rolling(window=3).std()
+df['prev_close'] = df['Close'].shift(1)
+df['volume_change'] = df['Volume'] - df['Volume'].shift(1)
+
+df.dropna(inplace=True)
 
 # Define features and target
-X = df[['Open', 'Close', 'Volume', 'hour', 'volatility']]
+X = df[['Open', 'Close', 'Volume', 'hour', 'volatility', 'price_change', 'percent_change', 'rolling_mean_3', 'rolling_std_3', 'prev_close', 'volume_change']]
 y = df['Close'].shift(-1)
 
 # Drop last row (NaN target)
